da407043e8a1068b14944c6acdf0a4793722987e
mayn_ku <maynku07@gmail.com>
2026-03-23 04:38:30 +0530

    Resolve conflict: backend/package.json

--- backend/src/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose')
-async function main() {
-    await mongoose.connect(process.env.DB_CONNECT_URL)
-    console.log("Database connected successfully");
-}
-
-module.exports=main;
